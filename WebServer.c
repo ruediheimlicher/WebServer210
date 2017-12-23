3e8af29be53bf072842b77ca49e18a33385e35ed
Ruedi Heimlicher <r.heimlicher@bluewin.ch>
2017-12-23 17:42:53 +0100

    l 1064: Test für Kontrolle eeprom.pl: permanent auf 1 fixiert

--- WebServer.c
+++ WebServer.c
@@ -1059,6 +1059,10 @@ uint8_t analyse_get_url(char *str)	// codesnippet von Watchdog
                   {
                      permanent = strtol(actionbuf,NULL,16);                     
                   }
+                  
+                  // Test bei Umstellung, kontroille eeprom.pl
+                  permanent = 1;
+                  
                   if (permanent)
                   {
                      webtaskflag = EEPROMWRITETASK;               // Task fuer permanent setzen
